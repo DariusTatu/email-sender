33480ec9556337efbc73db5bbd1f5232576d2359
DariusTatu <daryus942007@gmail.com>
2023-05-02 18:13:58 +0300

    fixing comments

--- subscriber/views.py
+++ subscriber/views.py
@@ -3,19 +3,23 @@ from django.core.mail import send_mail
 from django.conf import settings
 import json
 
+# Shows the main web page
+
 
 def show_main_page(request):
     return render(request, "main.html")
 
+# Sends the email to the defined list from settings.py
+
 
 def send_email(request):
-    title = "Get the chance to win 1 million $ by clicking this link"
-    body = "Hello dear followers, you can follow me on my social media for a chance to win 1 million $."
+    title = "Bun venit la cafelutza DevOpsista"
+    body = "In aceasta seara la ora 18:30 ne intalnim, prieteni!"
 
     send_mail(
         subject=title,
         message=body,
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=settings.RECIPIENT_ADDRESS)
-
+# Returns the email has been successfully sent message
     return render(request, 'success.html')
